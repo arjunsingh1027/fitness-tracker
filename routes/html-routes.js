b4816aa1ad3003044626b772754d1918ea76804c
arjunsingh <arjunksingh1027@gmail.com>
2021-01-08 01:07:28 -0700

    html routes start

---
+++ routes/html-routes.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const workout = require("../models/models.js");
+
+module.exports = function (app){
+
+    // display last workout
+
+    // create exercise
+
+    // display stats
+};
