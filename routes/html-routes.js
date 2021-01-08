9cd0a7f1d761e19dedd8557693e993864fe957bf
arjunsingh <arjunksingh1027@gmail.com>
2021-01-08 10:09:07 -0700

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,17 @@ const workout = require("../models/models.js");
 module.exports = function (app){
 
     // display last workout
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
     // create exercise
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
 
     // display stats
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "..public/stats.html"));
+    }); 
 };
