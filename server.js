dd22bdfe92acc44bca53add753aa5d85f0fb6d53
arjunsingh <arjunksingh1027@gmail.com>
2021-01-11 10:59:49 -0700

    deploy?

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.use(express.json());
 // Static for everything in public folder
 app.use(express.static("public"));
 // Connection for Heroku and Atlas
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // Routes
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
